b08042cd008e09da90800049c420c99efe43abe6
NatchaTpnw <moonjjk05@gmail.com>
2024-12-04 10:53:27 +0700

    LabSheet01

--- LadSheet01/src/Lab102.java
+++ LadSheet01/src/Lab102.java
@@ -1,10 +1,15 @@
-
+import java.util.Scanner;
 public class Lab102 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-
+		public static void main(String[] args) {
+			Scanner input = new Scanner(System.in);
+			System.out.print("Input te number of minute: ");
+			int minute = input.nextInt();
+			int years = minute / 525600;
+			int days = minute % 525600 / 1440;
+			System.out.println(minute+" minute is approximately "+years+" years and "+days+" days");
+			input.close();
+			
 	}
 
 }
